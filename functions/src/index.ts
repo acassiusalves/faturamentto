6c5e4c49c94440529d6947555fe2b534a4753fd9
Acassius Alves <acassiusalves@gmail.com>
2025-08-09 23:08:59 +0000

    src/index.ts(5,14): error TS2305: Module '"firebase-admin/auth"' has no

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,8 +1,8 @@
 
 import {initializeApp} from "firebase-admin/app";
 import {getAuth} from "firebase-admin/auth";
-import type {AuthError} from "firebase-admin";
 import {getFirestore} from "firebase-admin/firestore";
+import type {AuthError} from "firebase-admin";
 import * as logger from "firebase-functions/logger";
 import {HttpsError, onCall} from "firebase-functions/v2/https";
 
