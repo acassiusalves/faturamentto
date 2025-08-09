cfcce769461523b1e2f9390a71b54acb2264a3af
Acassius Alves <acassiusalves@gmail.com>
2025-08-09 15:42:58 +0000

    O SKU nunca pode se repetir e o sistema nao pode aceitar o mesmo produto

--- src/components/product-creator.tsx
+++ src/components/product-creator.tsx
@@ -1,3 +1,4 @@
+
 "use client";
 
 import { useState, useEffect, useMemo } from 'react';
@@ -533,3 +534,5 @@ export function ProductCreator({ category }: ProductCreatorProps) {
     </>
   );
 }
+
+    
