34d050f9450f81f587d2b494aa5a501e0debfbd8
Acassius Alves <acassiusalves@gmail.com>
2025-08-09 13:30:53 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- next.config.ts
+++ next.config.ts
@@ -16,6 +16,12 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'files-product.magalu.com',
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
   devIndicators: {
