20acd785d5fc00231da81f2ce5835d1687698447
Acassius Alves <acassiusalves@gmail.com>
2025-08-23 01:09:38 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- next.config.ts
+++ next.config.ts
@@ -1,3 +1,4 @@
+
 import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
@@ -22,6 +23,12 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'http',
+        hostname: 'http2.mlstatic.com',
+        port: '',
+        pathname: '/**',
+      }
     ],
   },
  devIndicators: {
