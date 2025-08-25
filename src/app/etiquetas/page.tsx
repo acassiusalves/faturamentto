418ab980928ce1e33dce20e8821cb8c0442a1d58
Acassius Alves <acassiusalves@gmail.com>
2025-08-25 01:13:04 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/app/etiquetas/page.tsx
+++ src/app/etiquetas/page.tsx
@@ -1,7 +1,7 @@
 
 "use client";
 
-import { useActionState, useEffect, useState } from 'react';
+import { useActionState, useEffect, useState, useTransition } from 'react';
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card';
 import { Label } from '@/components/ui/label';
 import { Input } from '@/components/ui/input';
