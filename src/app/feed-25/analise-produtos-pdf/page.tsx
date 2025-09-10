e127560a86168561e41223ca2d8add3eea7f183f
Acassius Alves <acassiusalves@gmail.com>
2025-09-10 22:59:23 +0000

    Try fixing this error: `Unhandled Runtime Error: Error: Element type is

--- src/app/feed-25/analise-produtos-pdf/page.tsx
+++ src/app/feed-25/analise-produtos-pdf/page.tsx
@@ -14,7 +14,6 @@ import { analyzeCatalogAction } from '@/app/actions';
 import { useToast } from '@/hooks/use-toast';
 import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from '@/components/ui/table';
 import { Badge } from '@/components/ui/badge';
-import Image from 'next/image';
 
 setupPdfjsWorker();
 
