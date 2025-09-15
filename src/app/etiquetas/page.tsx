c17d3444f7348f3d8c86c03563ee7e4ae3915bbf
Acassius Alves <acassiusalves@gmail.com>
2025-09-15 22:22:17 +0000

    Try fixing this error: `Unhandled Runtime Error: ReferenceError: DialogT

--- src/app/etiquetas/page.tsx
+++ src/app/etiquetas/page.tsx
@@ -21,6 +21,8 @@ import {
   DialogHeader,
   DialogTitle,
   DialogDescription,
+  DialogTrigger,
+  DialogFooter,
 } from '@/components/ui/dialog';
 import { Input } from "@/components/ui/input";
 import { DateRangePicker } from "@/components/ui/date-range-picker";
